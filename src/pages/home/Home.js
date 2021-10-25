44778d8ae6d57b3a63bf2708138aba7c30be63b5
unclebay <unclebigbay@gmail.com>
2021-10-25 16:04:05 +0100

    fixing missing files

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,12 +3,14 @@ import { PetitionSection } from '../../components/layouts/PetitionSection';
 import { Carousel } from '../../components/layouts/Carousel';
 import { NewsLetter } from '../../components/layouts/NewsLetter';
 import { Accordion } from '../../components/layouts/Accordion';
+import { Notice } from '../../components/layouts/Notice';
 
 export const Home = () => {
   return (
     <React.Fragment>
       <NewsLetter />
       <Carousel />
+      <Notice />
       <PetitionSection headingcenter />
       <Accordion heading={'Frequently Asked Questions'} headingcenter />
     </React.Fragment>
